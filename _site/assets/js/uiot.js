ce7d80243b8bb7604628daa266279e2d82764b28
03616585163 <jose.rafael@uiot.org>
2017-07-16 14:47:02 -0300

    First Commit

--- _site/assets/js/uiot.js
+++ _site/assets/js/uiot.js
@@ -1,19 +1,12 @@
 $(function() {
-    $('.menu-icon, .menu-icon2').on('click', function() {
-        $('.overlay').toggleClass('dbr');
-    });
+  $('.salgado').on('click', function(){
 
-    $('.smart-header h1').addClass('fadeIn move-top');
-    setTimeout(function(){
-      $('.smart-header p').addClass('fadeIn move-top');
-    }, 300);
-    setTimeout(function(){
-      $('.powerstrip').addClass('fadeIn')
-    }, 900);
+  eae = $(this).attr('class').split(' ')[1];
 
-    $(window).scroll(function() {
-        if ($(document).scrollTop() > 5) {
-            $('.downarrow').fadeOut(1000);
-        }
-    });
+  $('.' + eae).addClass('active');
+
+});
+  $('.overlayexit').on('click', function(){
+    $(".overlay").removeClass('active');
+  })
 })
